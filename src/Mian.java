53c75396268a26e787cd6a490a81c999f92d74a7
ashikmdshakil <ashikmdshakilpranto@gmail.com>
2021-05-16 14:13:09 -0700

    linked list complete

--- src/Mian.java
+++ src/Mian.java
@@ -4,6 +4,7 @@ public class Mian {
         list.addLast(100);
         list.addLast(200);
         list.addLast(300);
+        list.removeAt(1);
         list.print();
     }
 }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -22,6 +22,88 @@ public class MyLinkedList {
             this.tail = newNode;
         }
     }
+    public int findIndex(int value){
+        var node = this.head;
+        int i = 0;
+        int count = -1;
+        while(node != null){
+            if(node.getValue() == value){
+                count = i ;
+                break;
+            }
+            i++;
+            node = node.getNextNode();
+        }
+        return count;
+    }
+
+    public void addAt(int index, int number){
+        if(index == 0){
+            addFirst(number);
+        }else{
+            index = index - 1;
+        }
+        var node = this.head;
+        int i = 0;
+        while(node != null){
+            if(i == index){
+                var newNode = new Node(number);
+                var nextNode = node.getNextNode();
+                node.setNextNode(newNode);
+                newNode.setNextNode(nextNode);
+                break;
+            }
+            i++;
+            node = node.getNextNode();
+        }
+    }
+    public void removeFirst(){
+        this.head = this.head.getNextNode();
+    }
+
+    public void removeLast(){
+        var node = this.head;
+        while(node != null){
+            if(node.getNextNode() == this.tail){
+                node.setNextNode(null);
+                this.tail = null;
+                this.tail = node;
+            }
+            node = node.getNextNode();
+        }
+    }
+
+    public void removeAt(int index){
+        if(index == 0){
+            removeFirst();
+        }
+        else {
+            index = index - 1;
+            var node = this.head;
+            int i = 0;
+            while (node != null) {
+                if (index == i) {
+                    node.setNextNode(node.getNextNode().getNextNode());
+                    break;
+                }
+                i++;
+                node = node.getNextNode();
+            }
+        }
+    }
+
+    public boolean contains(int value){
+        var node = this.head;
+        boolean contains = false;
+        while(node != null){
+            if(node.getValue() == value) {
+                contains = true;
+                break;
+            }
+            node = node.getNextNode();
+        }
+        return contains;
+    }
 
     public void print(){
         Node node = this.head;
